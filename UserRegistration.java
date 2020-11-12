08c740b109c7401bd2c9a24a2264d28f3e916545
PVKS8192 <74343010+PVKS8192@users.noreply.github.com>
2020-11-12 15:23:33 +0530

    Add files via upload

---
+++ UserRegistration.java
@@ -0,0 +1,12 @@
+        //Do not change the package name
+
+
+
+public class UserRegistration {       //Do not change the class name
+
+ public static void main(String[] args) throws Exception{
+	    
+	 System.out.println("Hello World");
+
+ }
+}
